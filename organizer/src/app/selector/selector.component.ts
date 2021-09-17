cb4918d047515692763332af2d891ce16b911e5b
sivan2203 <sivan9458@gmail.com>
2021-09-17 23:26:32 +0700

    add project

---
+++ organizer/src/app/selector/selector.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import {DateService} from "../shared/date.service";
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.scss']
+})
+export class SelectorComponent{
+
+  constructor(public dateService: DateService) { }
+}
